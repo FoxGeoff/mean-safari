6f12bd63868651e71b51dec93839bb88afdc13a2
Geoff Fox <fox.geoffrey@gmail.com>
2022-11-09 15:18:51 -0800

    Improving the server.js Code

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 
 ## Project Introduction
 
-1. blar blar
+1. Original Code Ref: <https://github.com/PacktPublishing/Angular-and-Node.js---The-MEAN-Stack-Guide>
 
 ## Kanban Task: Chapter 3 : Adding NodeJS to our Project Video: Adding the Node Backend
 
@@ -59,3 +59,61 @@ server.listen(port);
 ```
 
 ### Task: Improving the server.js Code (video)
+
+```Javascript
+/**
+ * <https://github.com/PacktPublishing/Angular-and-Node.js---The-MEAN-Stack-Guide/tree/master/03.%20Adding%20NodeJS%20to%20our%20Project>
+ */
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
+
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+server.listen(port);
+```
--- server.js
+++ server.js
@@ -1,15 +1,60 @@
 /**
  * File server.js
- * Very basic server using Express framwork
+ * <https://github.com/PacktPublishing/Angular-and-Node.js---The-MEAN-Stack-Guide/tree/master/03.%20Adding%20NodeJS%20to%20our%20Project>
  */
 
-const http = require('http');
-const app = require('./backend/app');
+ const app = require("./backend/app");
+ const debug = require("debug")("node-angular");
+ const http = require("http");
 
-// use enviroment var in production or port
-const port =process.env.PORT || 3002;
 
-app.set('port', port);
-const server = http.createServer(app);
-console.log('starting server on ' + port);
-server.listen(port);
+  // Check valid number check for port
+ const normalizePort = val => {
+   var port = parseInt(val, 10);
+
+   if (isNaN(port)) {
+     // named pipe
+     return val;
+   }
+
+   if (port >= 0) {
+     // port number
+     return port;
+   }
+
+   return false;
+ };
+
+ // Log correct error msg
+ const onError = error => {
+   if (error.syscall !== "listen") {
+     throw error;
+   }
+   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+   switch (error.code) {
+     case "EACCES":
+       console.error(bind + " requires elevated privileges");
+       process.exit(1);
+       break;
+     case "EADDRINUSE":
+       console.error(bind + " is already in use");
+       process.exit(1);
+       break;
+     default:
+       throw error;
+   }
+ };
+
+ const onListening = () => {
+   const addr = server.address();
+   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+   debug("Listening on " + bind);
+ };
+
+ const port = normalizePort(process.env.PORT || "3000");
+ app.set("port", port);
+
+ const server = http.createServer(app);
+ server.on("error", onError);             //listen for error
+ server.on("listening", onListening);     // listener
+ server.listen(port);
